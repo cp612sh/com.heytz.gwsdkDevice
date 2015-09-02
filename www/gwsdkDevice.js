7f5a7871547d3b26b1e94b2c1cf890cae3e16ad5
陈东东 <kevinchen@infapp.com>
2015-09-02 10:44:04 +0800

    删除key字段

--- www/gwsdkDevice.js
+++ www/gwsdkDevice.js
@@ -1,6 +1,6 @@
 var exec = require('cordova/exec');
 
-exports.deviceControl = function ( appid,productKey,uid,token,mac,key,value , success, error) {
+exports.deviceControl = function ( appid,productKey,uid,token,mac,value , success, error) {
     exec(success, error, "gwsdkDevice", "deviceControl", [appid,productKey,uid,token,mac,value]);
 };
 
